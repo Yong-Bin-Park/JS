d6f52ed3d67615463edede7a0dfb13360787ac0d
Yong-Bin-Park <dydqls7592@gmail.com>
2022-04-29 20:03:37 +0900

    info

--- kor/Main.js
+++ kor/Main.js
@@ -2,8 +2,9 @@
 class Font {
     // 생성자
     constructor(num, sizeText) {
-    this.num = num; 
-    this.sizeText = sizeText; } 
+    this.num = num; //숫자
+    this.sizeText = sizeText; //폰트 크기
+    } 
 }
 
 // 배열을 섞어버리는 함수
@@ -199,12 +200,31 @@ $(document).ready(function () {
 
 $(document).ready(function () {
     $("#btn2").click(function () {
+        var info_lb = document.getElementById("b");
+        var info_rb = document.getElementById("b");
         sortArr = notSortArr;
         sortArr.sort(function (a, b) {
             return a - b;
         });
         for (var i = 0; i < 10; i++) {
             $(str[i]).html(sortArr[i] + "  ");
+            
+            if(sortArr[i]>sortArr[i+1]){
+                info_lb.innerText = 'pass(',i,') ',sortArr[i],'가',sortArr[i+1],'보다 크기 때문에 오른쪽으로 갈수록 값이 커지도록 정렬되어야 하므로 ',sortArr[i],'과 ',sortArr[i+1],' 이 자리교체 됌';
+            }
+            else {
+                info_rb.innerText = 'pass(',i,') ',sortArr[i],'가',sortArr[i+1],'보다 작기 때문에 오른쪽으로 갈수록 값이 커지도록 정렬되어야 하므로 ',sortArr[i],'과 ',sortArr[i+1],' 이 자리교체 되지 않음';
+            }
         }
+
+        
+    });
+});
+
+//설명버튼 클릭이벤트
+$(document).ready(function () {
+    $("#btn4").click(function () {
+        var info = document.getElementById("b");   //'b'라는 id를 가진 element를 가져옵니다.
+        info.innerHTML = '<b>이것은 버블정렬입니다.</b><br>버블정렬(bubble sort)이란 서로 인접한 2개의 레코드를 비교하여 크기가 순서대로 되어 있지 않으면 서로 교환하여 정렬하는 알고리즘입니다.<br><b>버블 정렬(bubble sort) 알고리즘의 특징</b><br>장점 : 구현이 매우 간단하다.<br>단점 : 1.순서에 맞지 않은 요소를 인접한 요소와 교환한다.<br>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp2.하나의 요소가 가장 왼쪽에서 가장 오른쪽으로 이동하기 위해서는 배열에서 모든 다른 요소들과 교환되어야 한다.<br>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp3.특히 특정 요소가 최종 정렬 위치에 이미 있는 경우라도 교환되는 일이 일어난다.<br><b>버블 정렬(bubble sort)의 시간복잡도</b><br>Best : O(n^2) , Avg : O(n^2) , Worst : O(n^2)'; 
     });
 });
